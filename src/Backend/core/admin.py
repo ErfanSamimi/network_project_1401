5d127234275c0d8f7f20d5b11fc245e37c9e0b0d
erfan samimi <erf80sami@gmail.com>
2022-12-10 22:45:51 +0330

    add models to admin page

--- src/Backend/core/admin.py
+++ src/Backend/core/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import ChatRoom
+from .models import ChatRoom, ChatRoom_Member, User, GroupChat, Channels, Message
 # Register your models here.
 
-admin.site.register(ChatRoom)
+class ChatRoomAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'chat_room_type', 'chat_room_id', 'create_date']
+
+class GroupChatAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'chat_room_type', 'chat_room_id', 'create_date', 'title']
+
+class ChatRoomMemberAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'member', 'role']
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'type', 'sender', 'send_date']
+
+admin.site.register(ChatRoom, ChatRoomAdmin)
+admin.site.register(ChatRoom_Member, ChatRoomMemberAdmin)
+admin.site.register(User)
+admin.site.register(GroupChat, GroupChatAdmin)
+admin.site.register(Channels, GroupChatAdmin)
+admin.site.register(Message, MessageAdmin)
+
